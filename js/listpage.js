d55a32f4fd9c245baa49794ae8b7b32b9fd7a1d5
kodyAgin <kodyagin@gmail.com>
2024-06-25 16:53:15 +0300

    creating catgroty button whit the functhions quickSearch showFilers

--- js/listpage.js
+++ js/listpage.js
@@ -5,12 +5,52 @@ window.onload = () => {
             initialize(data)
             document.getElementById("sourceBar").addEventListener("input",searchSoldiers);
         });
-    
+        document.getElementById("category_bt").onclick = showFilers;
+
+        document.getElementById("yearsInUnit").addEventListener("mouseover",function(event) {
+            document.getElementById("submenu").style.display = "block";
+            document.getElementById("submenu").onclick = quickSearch;
+        });
+        document.getElementById("yearsInUnit").addEventListener("mouseout", function(event) {
+            document.getElementById("submenu").style.display = "none";
+        });
+        document.getElementById("submenu").addEventListener("li", function(event){
+            
+            quickSearch(event.target)
+        });
+}
+
+function quickSearch(text) {
+    const main = document.getElementsByClassName("mainContainer")[0];
+    let article = main.getElementsByTagName("article");
+    const value = "שנים ביחידה:" + text.target.textContent;    
+    for(i = 0; i < article.length ;i++){
+        let x = article[i].querySelectorAll("ul")[0];
+        let y = x.children[2];
+        let z = y.children[0].textContent.trim(); 
+        if(z === value){   
+            continue
+        }else{
+            article[i].style.display = "none";
+        }
+    }       
+}
+function showFilers() {
+    const categoryMenu = document.getElementById("category_menu");
+    const main = document.getElementsByClassName("mainContainer")[0];
+    let article = main.getElementsByTagName("article");
+    if (!(categoryMenu.style.display === "block")) {
+        categoryMenu.style.display = "block";
+        for (let i = 0; i < article.length; i++) {
+            article[i].style.display = "flex";
+        }
+    }else{
+        categoryMenu.style.display = "none"
+    }
 }
 
 function initialize(data) {
     const main = document.getElementsByClassName("mainContainer")[0];
-    
     data.products.forEach(product => { 
         const article = document.createElement("article");
         let ul = document.createElement("ul");
@@ -23,8 +63,6 @@ function initialize(data) {
         li_soldierName.innerHTML = `<span>שם: ${product["soldier name"]}</span>`;
         li_role.innerHTML = `<span>תפקיד: ${product["role"]}</span>`;
         li_years.innerHTML= `<span> שנים ביחידה:${product["years in the unit"]}</span>`;
-
-        
         ul.appendChild(li_soldierName);
         ul.appendChild(li_role);
         ul.appendChild(li_years);
@@ -32,13 +70,21 @@ function initialize(data) {
         article.appendChild(soldierImg);
         main.appendChild(article);
     });
-    
+    document.getElementById("category_menu").style.display = "none";
+    document.getElementById("submenu").style.display = "none";
 }
+
 function searchSoldiers() {
     let inputField = document.getElementById("sourceBar");
     let value = "שם: "+inputField.value.toLowerCase();
     const main = document.getElementsByClassName("mainContainer")[0];
     let article = main.getElementsByTagName("article");
+    if(inputField.value.trim()===""){
+        for(i=0;i<article.length;i++){
+            article[i].style.display="flex";
+        }
+    }
+    
     for(i = 0; i < article.length ;i++){
         let x = article[i].querySelectorAll("ul")[0];
         let y = x.children[0];
@@ -47,6 +93,5 @@ function searchSoldiers() {
         }
     }
 }
-
 
 

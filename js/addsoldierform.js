b3568c1e13421af265fd833628e0cff38b6c672b
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-25 14:37:56 +0300

    added api url

--- js/addsoldierform.js
+++ js/addsoldierform.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.querySelectorAll('.navList a');
     const currentPage = window.location.pathname.split("/").pop();
-
+    
     navLinks.forEach(link => {
         if (link.getAttribute('href') === currentPage) {
             link.classList.add('active');
         }
-
-        link.addEventListener('click', function() {
+        
+        link.addEventListener('click', function () {
             navLinks.forEach(nav => nav.classList.remove('active'));
             this.classList.add('active');
         });
@@ -15,38 +15,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const FormHandler = {
         getFormData: () => {
-            const soldierName = document.getElementById('soldierName').value;
-            const personalNumber = document.getElementById('personalNumber').value;
+            const soldierID = document.getElementById('soldierID').value;
+            const name = document.getElementById('name').value;
             const role = document.getElementById('role').value;
             const rank = document.getElementById('rank').value;
-            const rifleman = document.getElementById('rifleman').value;
-            const birthdate = document.getElementById('birthdate').value;
-            const yearsInUnit = document.getElementById('yearsInUnit').value;
-            const profilePicture = document.getElementById('profilePicture').files[0];
-
+            const yearsInTheUnits = document.getElementById('yearsInTheUnits').value;
+            const riflery = document.getElementById('riflery').value;
+            const dateOfBirth = document.getElementById('dateOfBirth').value;
+            const s_img = document.getElementById('s_img').files[0];
+    
             const formData = new FormData();
-            formData.append('soldierName', soldierName);
-            formData.append('personalNumber', personalNumber);
+            formData.append('soldierID', soldierID);
+            formData.append('name', name);
             formData.append('role', role);
             formData.append('rank', rank);
-            formData.append('rifleman', rifleman);
-            formData.append('birthdate', birthdate);
-            formData.append('yearsInUnit', yearsInUnit);
-            if (profilePicture) {
-                formData.append('profilePicture', profilePicture);
-            }
-
+            formData.append('yearsInTheUnits', yearsInTheUnits);
+            formData.append('riflery', riflery);
+            formData.append('dateOfBirth', dateOfBirth);
+            formData.append('s_img', s_img);
+    
             return formData;
         }
     };
 
     const addSoldierForm = document.getElementById('add-soldier-form');
-
+    
     if (addSoldierForm) {
         addSoldierForm.addEventListener('submit', async (event) => {
             event.preventDefault();
 
-            const fileInput = document.getElementById('profilePicture');
+            const fileInput = document.getElementById('s_img');
             const file = fileInput.files[0];
             if (file && (file.size > 300 * 1024 || file.type !== 'image/png')) {
                 console.error('הקובץ חייב להיות מסוג PNG ובגודל עד 300KB.');
@@ -55,23 +53,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const formData = FormHandler.getFormData();
 
-            try {
-                const response = await fetch('/api/soldiers', {
+            try {3000
+                const response = await fetch('https://final-project-serverside.onrender.com/api/soldiers/addSoldier', {
                     method: 'POST',
                     body: formData
                 });
-
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
-
                 const result = await response.json();
-                if (result.success) {
-                    console.log('Soldier added successfully');
-                   
-                } else {
-                    console.error('Failed to add soldier');
-                }
+                console.log('Success:', result);
             } catch (error) {
                 console.error('Error:', error);
             }

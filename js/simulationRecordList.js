58b20ba9ab5080398179e6dfeccdaf03e427e378
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-08-02 22:37:03 +0300

    view + adding simulationRecord?

--- js/simulationRecordList.js
+++ js/simulationRecordList.js
@@ -2,8 +2,9 @@ window.onload = () => {
     fetch("https://final-project-serverside.onrender.com/api/soldiers/SimulationsRecords")
         .then(Response => Response.json())
         .then(data =>{
-                initialize(data)
+                initialize(data);
                 document.getElementById("simulationSourceBar").addEventListener("input",searchSoldiers);
+                createSimulationHolder(data);
         })
         .catch(error => console.error('Error fetching data:', error));
 
@@ -62,7 +63,7 @@ function afvSearch(afvToRescue) {
 }
 
 function getAFVs() {
-    let afvs = {};
+    let afv = {};
 
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -78,7 +79,9 @@ function getAFVs() {
     .then((result) => {
         const ul = document.getElementById("afvSubmenu");
         result = JSON.parse(result);
+        console.log(result);
         for(var afv in result) {
+            console.log(afv);
             var li = document.createElement("li");
             var span = document.createElement("span");
             li.appendChild(span.appendChild(document.createTextNode(result[afv]['afvToRescue'])));
@@ -105,92 +108,107 @@ function showFilers() {
 function initialize(data) {
     const main = document.getElementsByClassName("mainContainer")[0];
     data.forEach(product => { 
-        const article = document.createElement("article");
-        article.setAttribute('data-id', product['id']);
-        article.classList.add("simulationRecordPlaceholder");
-        let div = document.createElement("div");
-        div.classList.add("recordInfo");
-        let id = document.createElement("div");
-        let simulationID = document.createElement("div");
-        let date = document.createElement("div");
-        let video = document.createElement("div");
-        let simulationName = document.createElement("div");
-        let location = document.createElement("div");
-        let afvToRescue = document.createElement("div");
-        let RescueVehicle = document.createElement("div");
-        let difficulty = document.createElement("div");
-        let commanderName = document.createElement("div");
-        let driverName = document.createElement("div");
-        let safetyOfficerName = document.createElement("div");
-        let teamMember1Name = document.createElement("div");
-        let teamMember2Name = document.createElement("div");
-        let teamMember3Name = document.createElement("div");
+        const articleInfo = document.createElement("div");
+        articleInfo.classList.add("articleInfo");
+        const location = document.createElement("span");
+        location.innerText = "מיקום:"+`${product.location}`
+        const difficulty = document.createElement("span");
+        difficulty.innerText = "רמת קושי:"+ `${product.difficulty}`;
+        const date = document.createElement("span");
+        date.innerText = "תאריך:"+`${product.date}`;
+        const participants = document.createElement("span");
+        
+        const teamMembers = document.createElement("span");
+                
+        teamMembers.innerText = "אנשי צוות:"+ addTeamMembers(product);
+        const commander = document.createElement("span");
+        commander.innerText ="מפקד:"+ `${product.CommanderName}`;
+        const safetyOfficer = document.createElement("span");
+        safetyOfficer.innerText = "קצין בטיחות:" + `${product.SafetyOfficerName}`;
 
-        id.textContent = `ID: ${record['id']}`;
-        simulationID.textContent = `Simulation ID: ${record['simulationID']}`;
-        date.textContent = `Date: ${record['date']}`;
-        video.innerHTML = `<iframe src="${record['video']}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        simulationName.textContent = `Simulation Name: ${record['simulationName']}`;
-        location.textContent = `Location: ${record['location']}`;
-        afvToRescue.textContent = `AFV to Rescue: ${record['afvToRescue']}`;
-        RescueVehicle.textContent = `Rescue Vehicle: ${record['RescueVehicle']}`;
-        difficulty.textContent = `Difficulty: ${record['difficulty']}`;
-        commanderName.textContent = `Commander: ${record['CommanderName']}`;
-        driverName.textContent = `Driver: ${record['DriverName']}`;
-        safetyOfficerName.textContent = `Safety Officer: ${record['SafetyOfficerName']}`;
-        teamMember1Name.textContent = `Team Member 1: ${record['TeamMember1Name']}`;
-        teamMember2Name.textContent = `Team Member 2: ${record['TeamMember2Name']}`;
-        teamMember3Name.textContent = `Team Member 3: ${record['TeamMember3Name']}`;
-
-        div.appendChild(id);
-        div.appendChild(simulationID);
-        div.appendChild(date);
-        div.appendChild(video);
-        div.appendChild(simulationName);
-        div.appendChild(location);
-        div.appendChild(afvToRescue);
-        div.appendChild(RescueVehicle);
-        div.appendChild(difficulty);
-        div.appendChild(commanderName);
-        div.appendChild(driverName);
-        div.appendChild(safetyOfficerName);
-        div.appendChild(teamMember1Name);
-        div.appendChild(teamMember2Name);
-        div.appendChild(teamMember3Name);
-
-        article.appendChild(div);
-        main.appendChild(article);
+        articleInfo.appendChild(location);
+        articleInfo.appendChild(difficulty);
+        articleInfo.appendChild(date);
+        
+        articleInfo.appendChild(teamMembers);
+        articleInfo.appendChild(commander);
+        articleInfo.appendChild(safetyOfficer);
+        return articleInfo;
     });
     document.getElementById("simulationCategoryMenu").style.display = "none";
     document.getElementById("difficultySubmenu").style.display = "none";
     getAFVs();
 }
 
-function searchSoldiers() {
-    let inputField = document.getElementById("simulationSourceBar");
-    let value = inputField.value.toLowerCase();
-    const main = document.getElementsByClassName("mainContainer")[0];
-    let article = main.getElementsByTagName("article");
-    if(inputField.value.trim() === "" || inputField.value.trim().length === 0){
-        for(i=0;i<article.length;i++){
-            article[i].style.display="flex";
-        }
-    }
-    
-    for (let i = 0; i < article.length; i++) {
-        let x = article[i].getElementsByClassName("recordInfo")[0];
-        let commanderName = x.children[9].textContent.toLowerCase();
-        let driverName = x.children[10].textContent.toLowerCase();
-        let safetyOfficerName = x.children[11].textContent.toLowerCase();
-        let teamMember1Name = x.children[12].textContent.toLowerCase();
-        let teamMember2Name = x.children[13].textContent.toLowerCase();
-        let teamMember3Name = x.children[14].textContent.toLowerCase();
+function addTeamMembers(params) {
+    const teamMembers = [params.TeamMember1Name,params .TeamMember2Name, params.TeamMember3Name]
+        .filter(name => name !== null)
+        .join(", ");
+    return teamMembers;
+}
 
-        if (commanderName.includes(value) || driverName.includes(value) || safetyOfficerName.includes(value) ||
-            teamMember1Name.includes(value) || teamMember2Name.includes(value) || teamMember3Name.includes(value)) {
-            article[i].style.display = "flex";
-        } else {
-            article[i].style.display = "none";
-        }
+function getUserId() {
+    const aKeyValue = window.location.search.substring(1).split('&');
+    const userId = aKeyValue[0].split("=")[1];
+    return userId;
+}
+
+function createArticleInfo(product) {
+    const articleInfo = document.createElement("div");
+    articleInfo.classList.add("articleInfo");
+
+    const location = document.createElement("span");
+    location.innerText = "מיקום:" + `${product.location}`;
+    const difficulty = document.createElement("span");
+    difficulty.innerText = "רמת קושי:" + `${product.difficulty}`;
+    const date = document.createElement("span");
+    date.innerText = "תאריך:" + `${product.date}`;
+    const participants = document.createElement("span");
+
+    const teamMembers = document.createElement("span");
+    teamMembers.innerText = "אנשי צוות:" + addTeamMembers(product);
+    const commander = document.createElement("span");
+    commander.innerText = "מפקד:" + `${product.CommanderName}`;
+    const safetyOfficer = document.createElement("span");
+    safetyOfficer.innerText = "קצין בטיחות:" + `${product.SafetyOfficerName}`;
+
+    articleInfo.appendChild(location);
+    articleInfo.appendChild(difficulty);
+    articleInfo.appendChild(date);
+    articleInfo.appendChild(teamMembers);
+    articleInfo.appendChild(commander);
+    articleInfo.appendChild(safetyOfficer);
+
+    return articleInfo;
+}
+
+function createArticleContent(product) {
+    const ArticleContent = document.createElement("div");
+    ArticleContent.classList.add("ArticleContent");
+
+    const simulationVideo = document.createElement("iframe");
+    simulationVideo.src = product.video;
+    simulationVideo.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+    simulationVideo.allowFullscreen = true;
+
+    ArticleContent.appendChild(simulationVideo);
+    ArticleContent.appendChild(createArticleInfo(product));
+
+    return ArticleContent;
+}
+
+function createSimulationHolder(recordsData) {
+    console.log(recordsData);
+    const mainContainer = document.getElementsByClassName("mainContainer")[0];
+
+    for (let i = 0; i < recordsData.length; i++) {
+        const article = document.createElement("article");
+        const title = document.createElement("span");
+
+        title.innerText = recordsData[i].simulationName;
+        article.appendChild(title);
+
+        article.appendChild(createArticleContent(recordsData[i]));
+        mainContainer.appendChild(article);
     }
 }
